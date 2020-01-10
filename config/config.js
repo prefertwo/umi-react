95e4b762f77756a96751113ee5cb3506ea24e7b9
prefertwo <1522876658@qq.com>
2020-01-10 16:23:28 +0800

    项目初始化

---
+++ config/config.js
@@ -0,0 +1,20 @@
+export default {
+  /* 约定的存放页面代码的文件夹是 pages 还是 page。不配置是pages */
+  singular: true,
+  /* 插件 */
+  plugins: [
+    ['umi-plugin-react', {
+      /* 添加插件相关配置 */
+      antd: true
+    }]
+
+  ],
+  /* component 是相对于 page 目录的相对路径 */
+  routes: [
+    {
+      path: '/',
+      component: './HelloWord'
+    }
+  ]
+
+};
